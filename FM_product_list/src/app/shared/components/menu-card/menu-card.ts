7952c49250531223ff1cdc4b1a2e4fb13b9692fe
Mr-Dhie <drellolalia@gmail.com>
2025-07-20 21:44:38 +0800

    STYLE: improve UI responsiveness

--- FM_product_list/src/app/shared/components/menu-card/menu-card.ts
+++ FM_product_list/src/app/shared/components/menu-card/menu-card.ts
@@ -35,7 +35,7 @@ export class MenuCard {
     this.dessertRepository.addToCart(id);
   }
 
-  decreateItem(id:string){
+  decreaseItem(id:string){
     this.dessertRepository.decreaseCartItem(id)
   }
 
